06a56bd721b259eaed7f9abc68767f7929873cd6
Juhun Park <riann3207@gmail.com>
2023-09-07 21:59:17 -0400

    sqrtx

--- test_py2.py
+++ test_py2.py
@@ -1,27 +1,26 @@
 class Solution:
-    def rob(self, nums) -> int:
-        length = len(nums)
-        if length < 2:
-            return max(nums)
-        else:
-            dp = [0] * length
-            dp[0] = nums[0]
-            dp[1] = nums[1]
-    
-            for i in range(2, length):
-                dp[i] = nums[i] + max(dp[i - 3], dp[i - 2])
-                
-            return max(dp[-2], dp[-1])
-        
+    def mySqrt(self, x: int) -> int:
+        i = 0
+        while True:
+            temp = i**2
+            if x == 0:
+                return 0
+            else:
+                if temp == x:
+                    return i
+                elif temp > x:
+                    return i - 1
+                else:
+                    i += 1
 
 
 self = Solution()
 
-nums = [0]
-print(Solution.rob(self, nums))
+x = 4
+print(Solution.mySqrt(self, x))
 
-nums = [2,7,9,3,1]
-print(Solution.rob(self, nums))
+x = 8
+print(Solution.mySqrt(self, x))
 
-nums = [2,5,7,8,5,3,4,7,9,6,4,3]
-print(Solution.rob(self, nums))
+x = 0
+print(Solution.mySqrt(self, x))
