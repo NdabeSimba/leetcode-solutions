8a256c7c195bd03b93f728cc58e63091c50d687f
Juhun Park <riann3207@gmail.com>
2024-01-04 11:22:33 -0500

    kmp algorithm

--- test_code1.py
+++ test_code1.py
@@ -1,20 +1,8 @@
 class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        s_sum = 0
-        t_sum = 0
-
-        for i in s:
-            s_sum += ord(i)
-
-        for j in t:
-            t_sum += ord(j)
-
-        result = abs(s_sum - t_sum)
-        return chr(result)
-
+    def strStr(self, haystack: str, needle: str) -> int:
+        # kmp algorithm
+        return 
 
 self = Solution()
 
-s = "abc"
-t = "abcd"
-print(Solution.findTheDifference(self, s, t))
+
