459ee81659f92687e00f3a5ba5e6af658917d30d
Juhun Park <riann3207@gmail.com>
2024-01-07 07:47:02 -0500

    283

--- test_code1.py
+++ test_code1.py
@@ -1,8 +1,21 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        # kmp algorithm
-        return 
+    def moveZeroes(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        while True:
+            if len(nums) <= i:
+                break
+            print(i)
+            if nums[i] == 0:
+                nums.append(nums.pop(i))
+            else:
+                i += 1
+        
+        print(nums)
 
 self = Solution()
 
-
+nums = [0,1,0,3,12]
+Solution.moveZeroes(self, nums)
