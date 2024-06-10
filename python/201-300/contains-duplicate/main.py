662ae2de5328e0eec96ed98552c8dff54e87aeb4
Juhun Park <riann3207@gmail.com>
2024-06-10 04:42:46 -0400

    contains duplicate

---
+++ python/201-300/contains-duplicate/main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashtable = set()
+
+        for num in nums:
+            if num in hashtable:
+                return True
+            else:
+                hashtable.add(num)
+        
+        return False
